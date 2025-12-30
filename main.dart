776a20c0b52eccfa7c08730804096b05e613e968
Amazing Code (ac) <Shikhsulaimandiaa@gmail.com>
2025-12-30 09:29:20 +0100

    Update main.dart

--- main.dart
+++ main.dart
@@ -11,4 +11,7 @@ void main() { // Main function
 
     If you want to extend it, you need to create a class, for example, with the name MyHomePage. Then, in the main function, 
     you need to change the state from "home" to MyHomePage() so that the class takes over the rest of the work, such as scaffolding.
+
+    The shortest length is achieved by placing everything inside the curly braces of the `main` function, instead of placing them at the 
+    bottom, on a single line next to the function. This way, the entire function and its code are on one line. The total code then consists of two lines.
          */
