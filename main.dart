26e77b7ee32c101f9e79cb685705ddb3ebdd4cb6
Amazing Code (ac) <Shikhsulaimandiaa@gmail.com>
2025-12-30 09:24:40 +0100

    Create main.dart for basic Flutter app structure
    Initial Flutter app setup with a white background.

---
+++ main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart' // Library
+
+void main() { // Main function
+  runApp(const MaterialApp(home: Scaffold(backgroundColor: Colors.white);) 
+}
+
+         
+
+/*  info:
+    You can also increase the size of the code by not putting everything on one line.    
+
+    If you want to extend it, you need to create a class, for example, with the name MyHomePage. Then, in the main function, 
+    you need to change the state from "home" to MyHomePage() so that the class takes over the rest of the work, such as scaffolding.
+         */
